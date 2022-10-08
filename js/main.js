bdb32ee5548a347ca63940af86c27a88a46909e5
RomaDel1fe <romadelife@gmail.com>
2022-10-09 02:00:50 +0300

    added contact form

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 const burger = document.querySelector('.header__burger');
 const menuBody = document.querySelector('.header__menu');
+const formInput = document.querySelector('.form__input');
+const formLabel = document.querySelector('.form__label');
 
-document.addEventListener("click", menuNav)
+document.addEventListener("click", menuNav);
 
 function menuNav(event){
    if(event.target.closest('.header__burger')){
@@ -9,3 +11,15 @@ function menuNav(event){
       menuBody.classList.toggle('_active');
    }
 }
+
+// document.addEventListener("mouseover", formHover);
+
+// function formHover(event){
+//    if(event.target.closest('.form__input')){
+//       formLabel.classList.add('_hover');
+//    }
+//    else{
+//       formLabel.classList.remove('_hover');
+//    }
+// }
+
