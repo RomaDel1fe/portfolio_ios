408e38356fb62f382ee3f14f63764d598287d4b2
RomaDel1fe <romadelife@gmail.com>
2022-10-09 19:28:44 +0300

    added basic animation

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const burger = document.querySelector('.header__burger');
 const menuBody = document.querySelector('.header__menu');
 const formInput = document.querySelector('.form__input');
 const formLabel = document.querySelector('.form__label');
+const animItems = document.querySelectorAll('._anim');
 
 document.addEventListener("click", menuNav);
 
@@ -30,3 +31,35 @@ var minOffset = 55;
     } 
   }
 
+
+
+if (animItems.length > 0) {
+  window.addEventListener('scroll', animOnScroll);
+  function animOnScroll(){
+    for (let index = 0; index < animItems.length; index++){
+      const animItem = animItems[index];
+      const animItemHeight = animItem.offsetHeight;
+      const animItemOffset = offset(animItem).top;
+      const animStart = 4;
+
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+      if (animItemHeight > window.innerHeight){
+        animItemPoint = window.innerHeight - window.innerHeight / animStart;
+      }
+
+      if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)){
+        animItem.classList.add('_active');
+      } else{
+        animItem.classList.remove('_active');
+      }
+    }
+  }
+}
+function offset(el){
+  const rect = el.getBoundingClientRect(),
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  return { top: rect.top + scrollTop, left: rect.left + scrollLeft}
+}
+
+animOnScroll();
